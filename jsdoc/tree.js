034310090bc578029008b3371e6da90e7a814181
Brian Pontarelli <brian@inversoft.com>
2013-05-30 10:58:44 -0600

    Updated JSDoc

---
+++ jsdoc/tree.js
@@ -0,0 +1,107 @@
+var tree = [
+	["Global", "global.html", "", [
+		[
+	"Prime", 
+	"Prime.html", "", [
+  [
+	"Ajax", 
+	"Prime.Ajax.html", "", [
+  [
+	"Request", 
+	"Prime.Ajax.Request.html", "", [
+  
+	]
+]
+
+	]
+]
+, [
+	"Browser", 
+	"Prime.Browser.html", "", [
+  
+	]
+]
+, [
+	"Document", 
+	"Prime.Document.html", "", [
+  [
+	"Element", 
+	"Prime.Document.Element.html", "", [
+  
+	]
+]
+, [
+	"ElementList", 
+	"Prime.Document.ElementList.html", "", [
+  
+	]
+]
+
+	]
+]
+, [
+	"Effects", 
+	"Prime.Effects.html", "", [
+  [
+	"Appear", 
+	"Prime.Effects.Appear.html", "", [
+  
+	]
+]
+, [
+	"BaseTransition", 
+	"Prime.Effects.BaseTransition.html", "", [
+  
+	]
+]
+, [
+	"Fade", 
+	"Prime.Effects.Fade.html", "", [
+  
+	]
+]
+
+	]
+]
+, [
+	"Events", 
+	"Prime.Events.html", "", [
+  [
+	"Keys", 
+	"Prime.Events.Keys.html", "", [
+  
+	]
+]
+
+	]
+]
+, [
+	"Template", 
+	"Prime.Template.html", "", [
+  
+	]
+]
+, [
+	"Utils", 
+	"Prime.Utils.html", "", [
+  
+	]
+]
+, [
+	"Widgets", 
+	"Prime.Widgets.html", "", [
+  [
+	"MultipleSelect", 
+	"Prime.Widgets.MultipleSelect.html", "", [
+  
+	]
+]
+
+	]
+]
+
+	]
+]
+
+	]]
+];
