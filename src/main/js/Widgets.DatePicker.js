4524edc60c3beca6eb56bc30ac9042ccb34048c7
Daniel DeGroff <djdegroff@gmail.com>
2015-08-06 18:43:53 -0600

    Stub out DatePicker widget

---
+++ src/main/js/Widgets.DatePicker.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2015, Inversoft Inc., All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+var Prime = Prime || {};
+
+/**
+ * The Prime.Widgets namespace.
+ *
+ * @namespace Prime.Widgets
+ */
+Prime.Widgets = Prime.Widgets || {};
+
+/**
+ * Constructs a new DatePicker object for the given input element.
+ *
+ * @param {Prime.Document.Element} element The Prime Element for the DatePicker widget.
+ * @constructor
+ */
+Prime.Widgets.DatePicker = function(element) {
+
+  this.element = (element instanceof Prime.Document.Element) ? element : new Prime.Document.Element(element.domElement);
+  if (!this.element.is('input')) {
+    throw new TypeError('You can only use Prime.Widgets.DatePicker with an input element');
+  }
+
+};
+
+Prime.Widgets.DatePicker.prototype = {
+
+  /**
+   * Destroys the DatePicker Widget
+   */
+  destroy: function() {
+  },
+
+  /* ===================================================================================================================
+   * Private methods
+   * ===================================================================================================================*/
+};
