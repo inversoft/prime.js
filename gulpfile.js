507d81f2d5a7dd4426fe38794ad9af2372f59b19
Daniel DeGroff <djdegroff@gmail.com>
2018-11-30 12:54:46 -0700

    Fix version for tests

--- gulpfile.js
+++ gulpfile.js
@@ -27,9 +27,10 @@ const sourceMaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const uglifyes = require('gulp-uglify-es').default;
 const process = require('process');
+const versionSuffix = typeof(process.env['project.version']) !== 'undefined' ? '-' + process.env['project.version'] : '';
 
 gulp.task('prime.js', ['prime-es6.js'], () =>
-    gulp.src(`build/prime-es6-${process.env['project.version']}.js`)
+    gulp.src(`build/prime-es6${versionSuffix}.js`)
         .pipe(sourceMaps.init({loadMaps: true}))
         .pipe(babel({
           presets: [
@@ -41,23 +42,23 @@ gulp.task('prime.js', ['prime-es6.js'], () =>
           ],
           sourceType: "script"
         }))
-        .pipe(rename(`prime-${process.env['project.version']}.js`))
+        .pipe(rename(`prime${versionSuffix}.js`))
         .pipe(sourceMaps.write())
         .pipe(gulp.dest('build')));
 
 gulp.task('prime-min.js', ['prime.js'], () =>
-    gulp.src(`build/prime-${process.env['project.version']}.js`)
+    gulp.src(`build/prime${versionSuffix}.js`)
         .pipe(sourceMaps.init({loadMaps: true}))
         .pipe(uglify())
-        .pipe(rename(`prime-min-${process.env['project.version']}.js`))
+        .pipe(rename(`prime-min${versionSuffix}.js`))
         .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('build')));
 
 gulp.task('prime-es6-min.js', ['prime-es6.js'], () =>
-    gulp.src(`build/prime-es6-${process.env['project.version']}.js`)
+    gulp.src(`build/prime-es6${versionSuffix}.js`)
         .pipe(sourceMaps.init({loadMaps: true}))
         .pipe(uglifyes())
-        .pipe(rename(`prime-es6-min-${process.env['project.version']}.js`))
+        .pipe(rename(`prime-es6-min${versionSuffix}.js`))
         .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('build')));
 
@@ -67,7 +68,7 @@ gulp.task('prime-es6.js', async () => {
   });
 
   await bundle.write({
-    file: `build/prime-es6-${process.env['project.version']}.js`,
+    file: `build/prime-es6${versionSuffix}.js`,
     format: 'iife',
     name: "Prime",
     indent: '\t',
@@ -81,15 +82,15 @@ gulp.task('minify-js', ['prime-min.js', 'prime-es6-min.js']);
 gulp.task('build-css', () =>
     gulp.src('./src/main/css/*.css')
         .pipe(sourceMaps.init())
-        .pipe(concat(`prime-${process.env['project.version']}.css`))
+        .pipe(concat(`prime${versionSuffix}.css`))
         .pipe(sourceMaps.write())
         .pipe(gulp.dest('./build')));
 
 gulp.task('minify-css', ['build-css'], () =>
-    gulp.src(`./build/prime-${process.env['project.version']}.css`)
+    gulp.src(`./build/prime${versionSuffix}.css`)
         .pipe(sourceMaps.init({loadMaps: true}))
         .pipe(css()) //also adds compatibility stuff.
-        .pipe(rename(`prime-min-${process.env['project.version']}.css`))
+        .pipe(rename(`prime-min${versionSuffix}.css`))
         .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('build')));
 
